49821a635db99035314aed92b1d05a774f61009d
Moath Obeidat <m.obeidat@elevatus.jobs>
2020-03-22 23:11:46 +0200

    first commit

---
+++ assets/js/jordan.js
@@ -0,0 +1,21 @@
+const api_url = 'https://corona.lmao.ninja/countries/jordan';
+	
+async function getData() {
+
+    const response = await fetch(api_url);
+    const data = await response.json();
+
+    $(".tConfirmed").text(data.cases);
+    $(".tActive").text(data.active);
+    $(".tDeaths").text(data.deaths);
+    $(".tRecovered").text(data.recovered);
+    $(".tCritical").text(Math.round(((data.recovered * 100) / data.cases))  + "%");
+    $('.tTodayCases').text(data.todayCases);
+    $('.tTodayDeaths').text(data.todayDeaths);
+    $('.tPersontage').text(Math.round(((data.deaths * 100) / data.cases))  + "%");
+
+        }
+
+        getData();
+
+        
